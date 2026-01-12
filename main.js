4acded73a762426fa43a34a05090aa4fef54008e
Roman Kozak <romankozak97.ua@gmail.com>
2026-01-12 18:46:01 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,94 @@
+const colors = {
+    BRACLAWSKIE: "#770c94",
+    RUSKIE: "#2563EB",
+    KIJOWSKIE: "#2c781d",
+    PODOLSKIE: "#ca7900",
+    WOLYNSKIE: "#eb2581",
+    DEFAULT: "gray"
+};
+
+// Init Info control
+let infoCtrl = L.control();
+
+infoCtrl.onAdd = function (map) {
+    this._div = L.DomUtil.get('info');
+    this.update();
+
+    return this._div;
+};
+
+infoCtrl.update = function (props) {
+    this._div.innerHTML = '<h2>Адмінподіл Речі Посполитої у 1620 році</h2>' +  (props ?
+        `${props.name} (${props.relation})`
+        : 'Наведіть курсор на регіон');
+};
+
+function setFeatureColor(relation) {
+    return  relation == 'Київське воєводство' ? colors.KIJOWSKIE :
+            relation == 'Брацлавське воєводство' ? colors.BRACLAWSKIE :
+            relation == 'Руське воєводство' ? colors.RUSKIE :
+            relation == 'Подільське воєводство' ? colors.PODOLSKIE :
+            relation == 'Волинське воєводство' ? colors.WOLYNSKIE :
+            colors.DEFAULT;
+}
+
+// Create layers
+const regionsLayer = L.geoJson(data, {
+    style: function (feature) {
+        return {
+            fillColor: setFeatureColor(feature.properties.relation),
+            weight: 2,
+            opacity: 0.7,
+            color: '#000',
+            fillOpacity: 0.2
+        };
+    },
+    onEachFeature: onEachFeature
+});
+
+const osmLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+
+// Init map
+let map = L.map('map', {
+    center: [48.88, 30.81],
+    zoom: 6,
+    layers: [osmLayer, regionsLayer]
+});
+
+infoCtrl.addTo(map);
+
+// Event listeners
+function highlightListener(e) {
+    let layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#000',
+        //fillColor: '#2563EB',
+        fillOpacity: 0.5
+    });
+
+    layer.bringToFront();
+    infoCtrl.update(layer.feature.properties);
+}
+
+function resetListener(e) {
+    regionsLayer.resetStyle(e.target);
+    infoCtrl.update();
+}
+
+function zoomToFeatureListener(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+// On each feature listener
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightListener,
+        mouseout: resetListener,
+        click: zoomToFeatureListener
+    });
+}
